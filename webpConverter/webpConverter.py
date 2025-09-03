83110121f569a58f30f5546ce8c9799e8d4917eb
yuzax <info@yusakukimura.com>
2025-09-03 21:30:52 +0900

    add dancing man

--- webpConverter/webpConverter.py
+++ webpConverter/webpConverter.py
@@ -6,11 +6,19 @@ IMG_FORMAT = "webp"
 ORIGINAL_DIR = "_toWebP/"
 CONVERT_DIR = "_WebP/"
 
-files = glob.glob(ORIGINAL_DIR + "*.jpg")
+files = glob.glob(ORIGINAL_DIR + "*.png")
 
 for file in files:
     file_name = os.path.splitext(os.path.basename(file))[0]
 
     image = Image.open(file)
-    image = image.convert("RGB")
+    
+    # 透過PNGの場合はRGBAモードを保持、そうでなければRGBに変換
+    if image.mode in ('RGBA', 'LA') or (image.mode == 'P' and 'transparency' in image.info):
+        # 透過情報を保持
+        image = image.convert("RGBA")
+    else:
+        # 透過情報がない場合はRGBに変換
+        image = image.convert("RGB")
+    
     image.save(CONVERT_DIR + file_name + '.webp', "webp")
